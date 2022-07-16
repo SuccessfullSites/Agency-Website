272707ccfdce5895f22b20153a0fe25e2d9b00a8
Dev_Shiv <im.shiv.579@gmail.com>
2022-07-16 18:30:17 +0530

    Styling: Features component.

--- components/Features.js
+++ components/Features.js
@@ -3,27 +3,31 @@ function Features() {
   const Container = document.createElement("div");
   Container.classList.add("container");
   Container.innerHTML = `
-    <div class="container-header">
-          <h1>Tailor-made features</h1>
-          <p>
-            Lorem ipsum is common placeholder text used to demonstrate the
-            graphic elements of a document or visual presentation.
-          </p>
-        </div>
-        <div class="container-body row row-cols-3 gy-5">
-          <div class="col">
-            <div class="icon">
-              <img src="" alt="" srcset="" />
-            </div>
-            <div class="text">
-              <div class="title">Robust workflow</div>
-              <div class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                erat nibh tristique ipsum.
-              </div>
-            </div>
-          </div>
-        </div>
+  <div
+  class="container-header text-center d-flex flex-column align-items-center"
+>
+  <h1 class="fw-bold">Tailor-made features</h1>
+  <p class="">
+    Lorem ipsum is common placeholder text used to demonstrate the
+    graphic elements of a document or visual presentation.
+  </p>
+</div>
+<div
+  class="container-body row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-4"
+>
+  <div class="col d-flex flex-column justify-content-center pt-5">
+    <div class="icon d-flex justify-content-center">
+      <img src="./Images/Base-feature-icon-1.svg" alt="" srcset="" />
+    </div>
+    <div class="text">
+      <div class="title fw-bold text-center">Robust workflow</div>
+      <div class="description text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        erat nibh tristique ipsum.
+      </div>
+    </div>
+  </div>
+</div>
     `;
   Features.appendChild(Container);
 }
