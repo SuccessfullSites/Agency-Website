342ab6833f9ad05283a21abfff480e2b96487e35
Dev_Shiv <im.shiv.579@gmail.com>
2022-07-17 15:29:16 +0530

    Adding: LogoCloud-6 to LogoCloud component.

--- components/LogoCloud.js
+++ components/LogoCloud.js
@@ -1,8 +1,11 @@
 function LogoCloud() {
-  const LogoCloud = document.getElementById("LogoCloud1");
-  const Container = document.createElement("div");
-  Container.classList.add("container", "border-top", "border-bottom", "py-4");
-  Container.innerHTML = `
+  const LogoCloud1 = document.getElementById("LogoCloud1");
+  const LogoCloud2 = document.getElementById("LogoCloud2");
+  const Container1 = document.createElement("div");
+  const Container2 = document.createElement("div");
+  Container1.classList.add("container", "border-top", "border-bottom", "py-4");
+  Container2.classList.add("container");
+  Container1.innerHTML = `
   <div
           class="row row-cols-2 row-cols-md-4 row-cols-lg-6 gy-5 justify-content-center"
         >
@@ -26,6 +29,21 @@ function LogoCloud() {
           </div>
         </div>
   `;
-  LogoCloud.appendChild(Container);
+  Container2.innerHTML = `
+  <div class="row row-cols-1 row-cols-md-2 gy-5 gx-5">
+          <div class="container-header col">
+            <div class="title fw-bold">Easy integrations with 170+ tools</div>
+            <p class="Description mt-2">
+              Connect Landify with your favourite tools that you use daily and
+              keep things on track.
+            </p>
+          </div>
+          <div class="content col">
+            <img class='img-fluid' src="./Images/mockup-metric06.png" alt="" srcset="" />
+          </div>
+        </div>
+  `;
+  LogoCloud1.appendChild(Container1);
+  LogoCloud2.appendChild(Container2);
 }
 LogoCloud();
