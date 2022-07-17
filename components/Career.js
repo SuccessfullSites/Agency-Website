566a5d8de8a8d638ae8f95b6a7193e8fa8a9d617
Dev_Shiv <im.shiv.579@gmail.com>
2022-07-17 16:13:19 +0530

    Adding: Careers component.

---
+++ components/Career.js
@@ -0,0 +1,27 @@
+function Career() {
+  const Career = document.getElementById("Careers");
+  const Container = document.createElement("div");
+  Container.classList.add("container", "py-5");
+  Container.innerHTML = `
+    <div class="row row-cols-1 row-cols-md-2 gy-5 align-items-center">
+    <div class="col container-header">
+      <div class="title fw-bold">Join the Atlassian Talent Community</div>
+      <p class="mt-2">
+        Don’t see an exact role match? No problem! Join our Talent
+        Community and stay up-to-date on company and careers updates
+        relevant to your career.
+      </p>
+      <a class="btn btn-primary mt-3" href="#"> Join Now </a>
+    </div>
+    <div class="col container-body">
+      <img
+        class="img-fluid"
+        src="./Images/career-illustration.png"
+        alt=""
+      />
+    </div>
+  </div>
+    `;
+  Career.appendChild(Container);
+}
+Career();
