e767676f857409e233c7e9ee2e75eacdc7d467bf
Dev_Shiv <im.shiv.579@gmail.com>
2022-07-21 23:28:15 +0530

    Styling: Footer component.

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,14 @@ function Footer() {
   const Container = document.createElement("div");
   Container.classList.add("container");
   Container.innerHTML = `
-  <div class="row">
-          <div class="col-10 left-section">
+  <div class="row gx-4 gy-5">
+          <div class="col-12 col-md-10 left-section ">
             <div class="brand-logo">
-              <a class="navbar-brand me-5" href="#">
-                <img class="BrandLogo" src="./Images/.svg" alt="" srcset="" />
+              <a class="navbar-brand" href="#">
+                <img class="BrandLogo" src="./Images/Logo-BgDark.svg" alt="" srcset="" />
               </a>
             </div>
-            <div class="nav-links d-flex flex-column">
+            <div class="nav-links d-flex flex-column mt-4">
               <ul class="nav justify-content-left row-1">
                 <li class="nav-item">
                   <a class="nav-link active" aria-current="page" href="#"
@@ -41,11 +41,11 @@ function Footer() {
                 </li>
               </ul>
             </div>
-            <div class="copyrights ps-3 pt-3">
-              © 2021 <a href="#">SuccessfullSite</a> Theme. All rights reserved
+            <div class="copyrights mt-4">
+              © 2021 <a class='text-decoration-underline' href="#">SuccessfullSite</a> Theme. All rights reserved
             </div>
           </div>
-          <div class="col-2 badges">
+          <div class="col-12 col-md-2 badges">
             <p>Get the App</p>
             <div class="badges-items me-3 d-none d-sm-block">
               <a class="badge-item text-decoration-none" href="#1">
@@ -58,7 +58,7 @@ function Footer() {
               </a>
               <a class="badge-item text-decoration-none" href="#2">
                 <img
-                  class="item"
+                  class="item mt-md-3"
                   src="./Images/Store badge-1.svg"
                   alt=""
                   srcset=""
